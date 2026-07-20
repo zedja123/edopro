d0a1b1e29f7ddd4da983676a5c92c1d566d0480b
zedja123 <miguel.zedja@hotmail.com>
2026-07-20 00:18:16 -0300

    Release 1.0.6 (Zedja-Cansado)

--- gframe/config.h
+++ gframe/config.h
@@ -15,41 +15,10 @@ extern bool show_changelog;
 #define EDOPRO_VERSION_MINOR 0
 #define EDOPRO_VERSION_PATCH 6
 #define MASQPRO_VERSION_STRING \
-STR(EDOPRO_VERSION_MAJOR) "." \
-STR(EDOPRO_VERSION_MINOR) "." \
-STR(EDOPRO_VERSION_PATCH)
+    STR(EDOPRO_VERSION_MAJOR) "." \
+    STR(EDOPRO_VERSION_MINOR) "." \
+    STR(EDOPRO_VERSION_PATCH)
 #define EDOPRO_VERSION_CODENAME "Zedja-Cansado"
-#define CONFIG_H
-
-compiler_features.h
-ocgapi_types.h
-text_types.h
-
-extern uint16_t PRO_VERSION;
-extern bool is_from_discord;
-extern bool open_file;
-extern epro::path_string open_file_name;
-extern bool show_changelog;
-
-#define EDOPRO_VERSION_MAJOR 1
-#define EDOPRO_VERSION_MINOR 0
-#define EDOPRO_VERSION_PATCH 6
-#define MASQPRO_VERSION_STRING \
-.
-.
-STR(EDOPRO_VERSION_PATCH)
-Zedja-Cansado
-.
-
-#define CLIENT_VERSION ((EDOPRO_VERSION_MAJOR & 0xff) | ((EDOPRO_VERSION_MINOR & 0xff) << 8) | ((OCG_VERSION_MAJOR & 0xff) << 16) | ((OCG_VERSION_MINOR & 0xff) << 24))
-#define GET_CLIENT_VERSION_MAJOR(ver) (ver & 0xff)
-#define GET_CLIENT_VERSION_MINOR(ver) ((ver >> 8) & 0xff)
-#define GET_CORE_VERSION_MAJOR(ver) ((ver >> 16) & 0xff)
-#define GET_CORE_VERSION_MINOR(ver) ((ver >> 24) & 0xff)
-#define EXPAND_VERSION(ver) GET_CLIENT_VERSION_MAJOR(ver), GET_CLIENT_VERSION_MINOR(ver), GET_CORE_VERSION_MAJOR(ver), GET_CORE_VERSION_MINOR(ver)
-
-#endif
-"
 #define EDOPRO_VERSION_STRING_DEBUG "MASQPro version " STR(EDOPRO_VERSION_MAJOR) "." STR(EDOPRO_VERSION_MINOR) "." STR(EDOPRO_VERSION_PATCH)
 #define EDOPRO_VERSION_STRING L"Masquerena's Club: EDOPro | " STR(EDOPRO_VERSION_MAJOR) "." STR(EDOPRO_VERSION_MINOR) "." STR(EDOPRO_VERSION_PATCH) " \"" EDOPRO_VERSION_CODENAME "\""
 #define CLIENT_VERSION ((EDOPRO_VERSION_MAJOR & 0xff) | ((EDOPRO_VERSION_MINOR & 0xff) << 8) | ((OCG_VERSION_MAJOR & 0xff) << 16) | ((OCG_VERSION_MINOR & 0xff) << 24))
@@ -60,22 +29,3 @@ Zedja-Cansado
 #define EXPAND_VERSION(ver) GET_CLIENT_VERSION_MAJOR(ver), GET_CLIENT_VERSION_MINOR(ver), GET_CORE_VERSION_MAJOR(ver), GET_CORE_VERSION_MINOR(ver)
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
